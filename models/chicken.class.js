6943da1ed4b33f47d4799746403064beddc33c15
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-19 23:54:07 +0100

    add animate

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,12 +1,28 @@
 class Chicken extends MovableObject{
     height = 70 
     width = 70
+    IMAGES_WALKING =[
+      'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+      'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+      'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+    ];
+    currentImage = 0;
   
      constructor(){
         super().loadImage('/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        this.loadImages(this.IMAGES_WALKING)
+        this.animate();
         this.x = 200 + Math.random() * 500;
         this.y = 490 - this.height;
 
     }
+    animate(){
 
-} 
+        setInterval(() => {
+             let i = this.currentImage % this.IMAGES_WALKING.length;
+             let path = this.IMAGES_WALKING[i]
+             this.img = this.imageCache[path];
+             this.currentImage++;
+        },200);
+
+} }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -4,9 +4,17 @@ class Cloud extends MovableObject {
     width = 600;
  constructor(){
         super().loadImage('/img/5_background/layers/4_clouds/1.png')
-        this.x = Math.random() * 500;
-       
 
+        this.x = Math.random() * 500;
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+                    this.x -= 0.2;
+
+            
+        }, 1000/ 60);
     }
 
 
