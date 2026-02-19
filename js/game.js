e1cc738fc13821d072b440e7597ba33f4304a231
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-19 10:16:57 +0100

    js: add charakter img

--- js/game.js
+++ js/game.js
@@ -3,6 +3,6 @@ let character = new Image();
 
 function init(){
     canvas = document.getElementById("canvas")
-    character.src = 'img/'
+    character.src = 'img/2_character_pepe/1_idle/idle/I-1.png'
 
 }
