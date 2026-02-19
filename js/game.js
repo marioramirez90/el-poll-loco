0f27d2fdf2397d220826ed1dc9df2bdd0cc6a46b
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-19 11:10:05 +0100

    js: add ctx

--- js/game.js
+++ js/game.js
@@ -1,8 +1,16 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init(){
-    canvas = document.getElementById("canvas")
-    character.src = 'img/2_character_pepe/1_idle/idle/I-1.png'
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
 
+    character.src = '../img/2_character_pepe/1_idle/idle/I-1.png';
+    ctx.drawImage(character, 20, 20, 50, 150);
+
+
+
+
+   
 }
