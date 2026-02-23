66a39e44ad91135ea316c878ed75e169ba9bd520
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-23 10:29:14 +0100

    add level link in html

---
+++ level/level1.js
@@ -0,0 +1,38 @@
+const level1 = new Level(
+    [
+    new Chicken(),
+    new Chicken(),
+    new Chicken(),
+],
+[
+    new Cloud(),
+],
+ [
+    
+    new BackgroundObject('img/5_background/layers/air.png',-960),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -960),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -960),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -960),
+
+    new BackgroundObject('img/5_background/layers/air.png',0),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+
+    new BackgroundObject('img/5_background/layers/air.png',959),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 959),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 959),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 959),
+
+    new BackgroundObject('img/5_background/layers/air.png',959*2),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 959*2),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 959*2),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 959*2),
+
+     new BackgroundObject('img/5_background/layers/air.png',959*3),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 959*3),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 959*3),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 959*3),
+
+]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -1,42 +1,9 @@
 class World{
 
 character = new Character();
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken(),
-];
-clouds =[
-    new Cloud(),
-]
-backgroundObjects = [
-    
-    new BackgroundObject('img/5_background/layers/air.png',-960),
-    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -960),
-    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -960),
-    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -960),
-
-    new BackgroundObject('img/5_background/layers/air.png',0),
-    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-
-    new BackgroundObject('img/5_background/layers/air.png',959),
-    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 959),
-    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 959),
-    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 959),
-
-    new BackgroundObject('img/5_background/layers/air.png',959*2),
-    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 959*2),
-    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 959*2),
-    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 959*2),
-
-     new BackgroundObject('img/5_background/layers/air.png',959*3),
-    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 959*3),
-    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 959*3),
-    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 959*3),
-
-]
+enemies = level1.enemies
+clouds = level1.clouds
+backgroundObjects = level1.backgroundObjects
 
 keyboard;
 canvas;
