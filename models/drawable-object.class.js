88f00c4a4a0b26f4177ec079bffe4d1c7b63947c
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-25 10:26:08 +0100

    move img;     imageCache =[]     currentImage = 0;      x = 120;     y = 200;       height = 150;     width = 100; to drawable-object.class.js

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,3 +1,26 @@
 class DrawableObject {
-    
+    img;
+    imageCache =[]
+    currentImage = 0;
+     x = 120;
+    y = 200;
+      height = 150;
+    width = 100;
+
+        loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+      draw(ctx){
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height,);
+    }
+
+       playaAnimation(images){
+        let i = this.currentImage % images.length;
+        let path = images[i]
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject{
-    x = 120;
-    y = 200;
-    img;
-    height = 150;
-    width = 100;
-    imageCache =[]
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.2;
     otherDiretion = false;
     speedY = 0.15;
@@ -27,15 +20,6 @@ class MovableObject{
         return this.y < 210; 
     }
 
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx){
-      ctx.drawImage(this.img, this.x, this.y, this.width, this.height,);
-    }
-
     drawframe(ctx){
         if(this instanceof Character || this instanceof Chicken ||  this instanceof Endboss){
         ctx.beginPath();
@@ -52,13 +36,6 @@ class MovableObject{
             img.src = path;
             this.imageCache[path] = img; 
         });
-    }
-
-    playaAnimation(images){
-        let i = this.currentImage % images.length;
-        let path = images[i]
-        this.img = this.imageCache[path];
-        this.currentImage++;
     }
 
     moveRight() {
