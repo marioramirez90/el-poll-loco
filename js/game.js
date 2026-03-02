89925557aec54daf8127c378cc35f5f9d4e78d63
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-02 12:46:21 +0100

    add fullscren and reset butten

--- js/game.js
+++ js/game.js
@@ -1,91 +1,83 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
-let canvasStart = document.getElementById("canvas")
-let startAudio = new Audio('audio/game/gameStart.mp3')
-let dialog = document.getElementById('dialog')
+let startAudio = new Audio("audio/game/gameStart.mp3");
+let dialog = document.getElementById("dialog");
+let fullscreen = document.getElementById("fullscreen");
+let restart = document.getElementById('reset')
 
-
-
-function startGame(){
-    initLevel1();
-    canvas = document.getElementById("canvas");
-    world = new World(canvas, keyboard);
-    removeStartMenu()
-
+function startGame() {
+  initLevel1();
+  canvas = document.getElementById("canvas");
+  world = new World(canvas, keyboard);
+  removeStartMenu();
 }
 
-function removeStartMenu(){
-    let start = document.getElementById("menu")
-    start.classList.add("d-none");
-    canvas.classList.remove("d-none");
-    startAudio.play()
+function removeStartMenu() {
+  let start = document.getElementById("menu");
+  start.classList.add("d-none");
+  restart.classList.remove("d-none")
+  canvas.classList.remove("d-none");
+  startAudio.play();
+  fullscreen.classList.remove("d-none");
 }
 
 function openDialog() {
-dialog.showModal();
+  dialog.showModal();
 }
 
-function clodeDialog(){
-dialog.close()
+function closeDialog() {
+  dialog.close();
 }
- var elem = document.getElementById("canvas");
+
 function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
+  if (canvas.requestFullscreen) {
+    canvas.requestFullscreen();
+  } else if (canvas.webkitRequestFullscreen) {
+    canvas.webkitRequestFullscreen();
+  } else if (canvas.msRequestFullscreen) {
+    canvas.msRequestFullscreen();
   }
 }
 
-
-
-window.addEventListener("keydown",(e) =>{
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = true;
-    } 
-    if (e.keyCode == 37) {
-        keyboard.LEFT = true;
-    } 
-     if (e.keyCode == 38) {
-        keyboard.UP = true;
-    } 
-    if (e.keyCode == 40) {
-        keyboard.DOWN = true;
-    } 
-      if (e.keyCode == 32) {
-        keyboard.SPACE = true;
-    } 
-      if (e.keyCode == 68) {
-        keyboard.D = true;
-    } 
-    console.log(keyboard.D);
-    
-    
-
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = true;
+  }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = true;
+  }
+  if (e.keyCode == 38) {
+    keyboard.UP = true;
+  }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = true;
+  }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = true;
+  }
+  if (e.keyCode == 68) {
+    keyboard.D = true;
+  }
+  console.log(keyboard.D);
 });
-window.addEventListener("keyup",(e) =>{
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = false;
-    } 
-    if (e.keyCode == 37) {
-        keyboard.LEFT = false;
-    } 
-     if (e.keyCode == 38) {
-        keyboard.UP = false;
-    } 
-    if (e.keyCode == 40) {
-        keyboard.DOWN = false;
-    } 
-      if (e.keyCode == 32) {
-        keyboard.SPACE = false;
-    } 
-       if (e.keyCode == 68) {
-        keyboard.D = false;
-    } 
-    
-    
-
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode == 39) {
+    keyboard.RIGHT = false;
+  }
+  if (e.keyCode == 37) {
+    keyboard.LEFT = false;
+  }
+  if (e.keyCode == 38) {
+    keyboard.UP = false;
+  }
+  if (e.keyCode == 40) {
+    keyboard.DOWN = false;
+  }
+  if (e.keyCode == 32) {
+    keyboard.SPACE = false;
+  }
+  if (e.keyCode == 68) {
+    keyboard.D = false;
+  }
 });
