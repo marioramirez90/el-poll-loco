487a88dc8d6885397831c54501098e3740a8c07f
marioramirez90 <mario-ramirez@hotmail.de>
2026-03-05 18:16:34 +0100

    add jsdoc, person and sudent jsand gitignor

---
+++ classes/student.class.js
@@ -0,0 +1,13 @@
+/**
+ * Repräsentiert einen Studenten.
+ */
+class Student {
+  /**
+   * @param {string} name Name des Studenten
+   * @param {number} age Alter des Studenten
+   */
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
